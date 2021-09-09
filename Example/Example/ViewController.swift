aedc0360183c6802deee47ae1252dc92a185c3d5
Habibur Rahamn <shopon2024@gmail.com>
2021-09-10 00:14:10 +0600

    Add example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Habibur Rahman on 9/9/21.
+//
+
+import UIKit
+import FloatingLabelTextFieldSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
---
+++ Example/Pods/Target Support Files/FloatingLabelTextFieldSwift/FloatingLabelTextFieldSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FloatingLabelTextFieldSwift : NSObject
+@end
+@implementation PodsDummy_FloatingLabelTextFieldSwift
+@end
