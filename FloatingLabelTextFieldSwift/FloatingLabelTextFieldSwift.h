3df8b33cc734d2c0970fc43d98c3fb87b127fdd3
Habibur Rahamn <shopon2024@gmail.com>
2021-09-09 19:22:14 +0600

    first commit

---
+++ FloatingLabelTextFieldSwift/FloatingLabelTextFieldSwift.h
@@ -0,0 +1,18 @@
+//
+//  FloatingLabelTextFieldSwift.h
+//  FloatingLabelTextFieldSwift
+//
+//  Created by Habibur Rahman on 9/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloatingLabelTextFieldSwift.
+FOUNDATION_EXPORT double FloatingLabelTextFieldSwiftVersionNumber;
+
+//! Project version string for FloatingLabelTextFieldSwift.
+FOUNDATION_EXPORT const unsigned char FloatingLabelTextFieldSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingLabelTextFieldSwift/PublicHeader.h>
+
+
